fd5a668df1147c59ed68134818073ca85b0ecf06
jz <macgyver@users.noreply.github.com>
2017-03-31 09:44:09 -0400

    also disable gesture nav in chrome CANARY

--- run-once.sh
+++ run-once.sh
@@ -1,5 +1,6 @@
 # disable two-finger swipe navigation in Chrome http://apple.stackexchange.com/a/80163/62458
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
+defaults write com.google.Chrome.canary.plist AppleEnableSwipeNavigateWithScrolls -bool FALSE
 # add me to wheel and _www for apache
 sudo dseditgroup -o edit -a jz -t user wheel
 sudo dseditgroup -o edit -a jz -t user _www
