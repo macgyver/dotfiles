aa405491694685808033c81f095b6e472821a5a1
jz <macgyver@users.noreply.github.com>
2017-05-16 18:42:46 -0400

    updated scroll gesture shit, removed powerline fonts

--- run-once.sh
+++ run-once.sh
@@ -1,6 +1,9 @@
 # disable two-finger swipe navigation in Chrome http://apple.stackexchange.com/a/80163/62458
-defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
-defaults write com.google.Chrome.canary.plist AppleEnableSwipeNavigateWithScrolls -bool FALSE
+defaults write com.google.Chrome        AppleEnableSwipeNavigateWithScrolls      -bool FALSE
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls      -bool FALSE
+# todo: these mouse swipe ones don't seem to work
+defaults write com.google.Chrome        AppleEnableMouseSwipeNavigateWithScrolls -bool FALSE
+defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool FALSE
 # add me to wheel and _www for apache
 #sudo dseditgroup -o edit -a jz -t user wheel
 #sudo dseditgroup -o edit -a jz -t user _www
@@ -9,13 +12,9 @@ defaults write com.google.Chrome.canary.plist AppleEnableSwipeNavigateWithScroll
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
 brew install zsh git diff-so-fancy watchman
 brew cask install flux phoenix qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
-# powerline fonts for agnoster theme
-git clone https://github.com/powerline/fonts.git powerline-fonts
-./powerline-fonts/install.sh
-rm -rf powerline-fonts
 
 echo "Install driver for xbox 360 controllers https://github.com/360Controller/360Controller/releases"
-echo "Install git diff enhancement https://github.com/so-fancy/diff-so-fancy"
 echo "Install SF Mono font system-wide https://medium.com/@deepak.gulati/using-sf-mono-in-emacs-6712c45b2a6d"
